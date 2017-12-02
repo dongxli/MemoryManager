5202663013e86f746559664961401ddd58df9919
Dongxuan Li <David@dongxuans-air.att.net>
2017-12-01 16:33:55 -0800

    Initial Commit

---
+++ MemoryManager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MemoryManager
+//
+//  Created by Dongxuan Li on 12/1/17.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
